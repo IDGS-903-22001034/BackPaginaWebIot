74cf288db81e30586953e0cac9a6c48bd89e982d
IDGS-803-22001034 <80085@alumnos.utleon.edu.mx>
2025-07-31 00:26:59 -0600

    Agregar archivos de proyecto.

---
+++ AuthAPI/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.Metadata;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthAPI.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string? FullName { get; set; }
+
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
